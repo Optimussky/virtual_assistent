4286347443f784b0bf2fa2636560fb7bc92090c7
Alberto Romero <optimussky05@gmail.com>
2022-10-05 17:54:42 -0500

    Add audiobook.py

---
+++ audiobook.py
@@ -0,0 +1,21 @@
+#Programa para crear un audiolibro
+
+import pyttsx3
+import PyPDF2 # pip install PyPDF2
+
+with open('firstAudioBookV.0.1.pdf','rb') as book:
+
+    full_text = ""
+
+    reader = PyPDF2.PdfFileReader(book)
+
+    audio_reader = pyttsx3.init()
+    audio_reader.setProperty("rate",100)
+
+    for page in range(reader.numPages):
+        next_page = reader.getPage(page)
+        content = next_page.extractText()
+        full_text += content
+    
+    audio_reader.save_to_file(full_text, "myfirstaudiobook.mp3")
+    audio_reader.runAndWait()
